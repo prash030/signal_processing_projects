a8dbef16754710d516ec435f286c58c0557ec3cc
Prash Ganesan <43354782+prash030@users.noreply.github.com>
2019-11-24 13:23:22 -0800

    Update ECG_signal_processing.m

--- ECG_signal_processing.m
+++ ECG_signal_processing.m
@@ -1,4 +1,5 @@
-% Author: Prasanth "Prash" Ganesan <prasganesan.pg@gmail.com>
+% Copyright (c) Prasanth "Prash" Ganesan
+% Author email: <prasganesan.pg@gmail.com>
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
